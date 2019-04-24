de6e296911306b90997c2061d3202f5236b50164
ukaye <itaukemeabasi@gmail.com>
2019-04-24 12:12:17 +0100

    remita debugging

--- helper-functions.js
+++ helper-functions.js
@@ -135,7 +135,7 @@ functions.authorizeMandate = function (payload, callback) {
         date = new Date(),
         datetime = date.getTime();
     console.log(datetime)
-    headers.REQUEST_ID = datetime;
+    headers.REQUEST_ID = 1556104246124;
     headers.API_KEY = process.env.REMITA_API_KEY;
     headers.MERCHANT_ID = process.env.REMITA_MERCHANT_ID;
     headers.API_DETAILS_HASH = SHA512(headers.API_KEY + headers.REQUEST_ID + process.env.REMITA_API_TOKEN);
