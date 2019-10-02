a7ca99208f3bc6660fce181b2966370930483c94
ukaye <itaukemeabasi@gmail.com>
2019-10-02 17:38:38 +0100

    client api part payment debugging

--- routes/service/custom-services/client.service.js
+++ routes/service/custom-services/client.service.js
@@ -2462,7 +2462,7 @@ router.post('/invoice/part-payment/:id/:invoice_id', helperFunctions.verifyJWT, 
             amount = parseFloat(req.body.amount) * 100,
             interest_amount = (req.body.amount >= invoice_.interest_owed)? invoice_.interest_owed : req.body.amount,
             principal_amount = (req.body.amount > interest_amount)? (req.body.amount - interest_amount) : 0;
-        
+        console.log((invoice_.interest_owed + invoice_.principal_owed).round(2))
         if (req.body.amount > (invoice_.interest_owed + invoice_.principal_owed).round(2))
             return res.send({
                 "status": 500,
