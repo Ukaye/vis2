31b1ac0b5a795076f553ad297bf5edb623280b86
unknown <fakuadesegun@live.com>
2019-10-04 14:25:03 +0100

    update

--- views/investment/statement/statement.js
+++ views/investment/statement/statement.js
@@ -18,7 +18,7 @@ async function getStatements(investmentId, startDate, endDate) {
                     $('#balanceHeader').html(`Opening Balance@ ${data[0].txn_date}`);
                     $('#balanceAmount').html(`${formater(data[0].balance)}`);
                     $('#openingBal').html(`${formater(data[0].balance)}`);
-                    $('#closingBal').html(`${formater(data[data.length-1].balance)}`);
+                    $('#closingBal').html(`${formater(data[data.length - 1].balance)}`);
                     $('#clientName').html(`${data[0].fullname}`);
                     $('#inventmentName').html(`${data[0].name}`);
                     $('#maturityDate').html(`${data[0].investment_mature_date}`);
@@ -39,11 +39,29 @@ async function getStatements(investmentId, startDate, endDate) {
                         }
                         $('#statementTable').append(
                             `<tr class="item">
-                            <td style="font-size:10px;" class="col-6">${x.description}</td>
-                            <td style="color:green;text-align: right;font-size: 10px;" class="col-3">${(x.is_credit===1)?(formater(x.amount.split(',').join('')).includes('.')?formater(x.amount.split(',').join('')):
-                            formater(x.amount.split(',').join(''))+'.00'):''}</td>
-                            <td style="text-align:right;color:red;text-align: right;font-size: 10px;" class="col-3">${(x.is_credit===0)?(formater(x.amount.split(',').join('')).includes('.')?formater(x.amount.split(',').join('')):
-                            formater(x.amount.split(',').join(''))+'.00'):''}</td>
+                                <td>
+                                ${x.description}
+                                </td>
+                                <td>
+                                    ${x.ref_no}
+                                </td>
+                                <td>
+                                    ${x.created_date}
+                                </td>
+                                <td>
+                                ${x.txn_date}
+                                </td>
+                                <td>
+                                ${(x.is_credit === 1) ? '₦' + (formater(x.amount.split(',').join('')).includes('.') ? formater(x.amount.split(',').join('')) :
+                                formater(x.amount.split(',').join('')) + '.00') : ''}
+                                </td>
+                                <td>
+                                ${(x.is_credit === 0) ? '₦' + (formater(x.amount.split(',').join('')).includes('.') ? formater(x.amount.split(',').join('')) :
+                                formater(x.amount.split(',').join('')) + '.00') : ''}
+                                </td>
+                                <td>
+                                    ₦${x.balance}
+                                </td>
                             </tr>`
                         );
                     });
@@ -78,7 +96,7 @@ function getConfigInfo() {
                 $('#address').html(data[0].address);
                 $('#email').html(data[0].email);
                 $('#phone').html(data[0].phone);
-                $('#state_poCode').html(`${data[0].state.toLowerCase().replace(data[0].state.toLowerCase()[0],data[0].state.toLowerCase()[0].toUpperCase())}, ${data[0].country} ${data[0].poBox}`);
+                $('#state_poCode').html(`${data[0].state.toLowerCase().replace(data[0].state.toLowerCase()[0], data[0].state.toLowerCase()[0].toUpperCase())}, ${data[0].country} ${data[0].poBox}`);
                 setTimeout(function () {
                     window.print();
                 }, 2000);
