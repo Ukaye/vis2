60dfc6aba8fc74b6647c2658910136138f04f2ee
realprinceviper <idrisadeniji01@gmail.com>
2019-12-13 10:12:00 -0500

    send mail data as object to api

--- routes/service/custom-services/atbmailer.service.js
+++ routes/service/custom-services/atbmailer.service.js
@@ -1,15 +1,45 @@
 const express = require('express');
 const router = express.Router();
-let fs = require('fs');
+const extract = require('mention-hashtag');
+const fs = require('fs');
+const axios = require('axios');
 
 router.post('/send', function(req, res, next) {
-    let action = req.params.action;
-    let data = req.body;
-   // if(action === 'atbmailerservice') {
-        res.json({
-            status: 'whoo'
-        })
-    //}
+    let mailData = req.body;
+
+    if(mailData.template !== '') {
+        fs.writeFileSync(`views/atbmailer/templates/${mailData.template}.html`, mailData.content);
+    }
+
+    //let mentionsQuery = 'select ';
+    const unsterilizedMsg = mailData.content;
+    let mentions = extract(unsterilizedMsg, { unique: false, symbol: false});
+    let mentionsQuery = `select ${mentions.join()}`;
+
+    console.log(mentionsQuery);
+    let { recipients, content, subject } = mailData;
+    let recipientsArray = (recipients).split(',');
+    let msg = {
+    to: recipientsArray,
+    from: 'noreply@atbtechsoft.com',
+    subject: subject,
+    text: 'hi',
+    html: content,
+    };
+
+    axios({
+        method: 'post',
+        url: 'http://localhost:5000/api/send',
+        data: msg
+    }).then(function(response) {
+        //console.log(response);
+        if(response.status === 200) {
+            res.json(response.data);
+        }
+    }).catch(function(error) {
+        //return error;
+        res.json(JSON.stringify(error));
+    });
 
 })
 
