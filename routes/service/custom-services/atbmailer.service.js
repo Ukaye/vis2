325dfa6e25eec5ec4ef9f112d2ba4423aa2e08a5
realprinceviper <idrisadeniji01@gmail.com>
2019-12-16 11:37:48 -0500

    send sterilized email with dynamic live data to email engine

--- routes/service/custom-services/atbmailer.service.js
+++ routes/service/custom-services/atbmailer.service.js
@@ -14,23 +14,10 @@ router.post('/send', function(req, res, next) {
 
     let sterilizedMsg = mailData.content;
     let mentions = extract(sterilizedMsg, { unique: true, symbol: false});
-    //if recipient is just one - proposal one
-/*     if(mentions.length > 0) {
-        let query = `select ${mentions.join()} from users where email = '${mailData.recipients}'`;
-        console.log(query);
-        db.query(query, function(error, results) {
-            if(error) {
-                console.log(error);
-            } else {
-                results.map(user => {
-                    console.log(user.fullname);
-                })
-                //console.log(results.username);
-            }
-        })
-    }  */
+    let { recipients, subject } = mailData;
+    let recipientsArray = (recipients).split(',');
 
-    //proposal 2
+    //proposal
     
     if(mentions.length > 0) {
         //let i = mentions.length - 1;
@@ -43,18 +30,40 @@ router.post('/send', function(req, res, next) {
                         console.log(error);
                     } else {
                         results.map(liveData => {
-                            console.log(liveData[element]);
+                            //console.log(liveData[element]);
                             //for(mentions[i] in unsterilizedMsg) 
                             let regex = new RegExp('@'+element, 'gi');
                             sterilizedMsg = sterilizedMsg.replace(regex, liveData[element]);
-                            //console.log(element);
-                            //console.log(sterilizedMsg);
                         });
                         i++;
 
-/*                         while(i == mentions.length) {
-                            console.log(sterilizedMsg);
-                        } */
+                        if(i === mentions.length) {
+                            //console.log(sterilizedMsg);
+
+                            let msg = {
+                                to: recipientsArray,
+                                from: 'noreply@atbtechsoft.com',
+                                subject: subject,
+                                text: 'hi',
+                                html: sterilizedMsg,
+                                };
+                            
+                                axios({
+                                    method: 'post',
+                                    url: 'http://localhost:5000/api/send',
+                                    data: msg
+                                }).then(function(response) {
+                                    //console.log(response);
+                                    if(response.status === 200) {
+                                        res.json(response.data);
+                                    }
+                                }).catch(function(error) {
+                                    //return error;
+                                    res.json(JSON.stringify(error));
+                                });
+
+                                console.log(sterilizedMsg);
+                        }
                     }
                 })
         });
@@ -65,32 +74,6 @@ router.post('/send', function(req, res, next) {
     //if recipients are more than one
 
     //db.query(query);
-
-    
-    let { recipients, content, subject } = mailData;
-    let recipientsArray = (recipients).split(',');
-    
-    let msg = {
-    to: recipientsArray,
-    from: 'noreply@atbtechsoft.com',
-    subject: subject,
-    text: 'hi',
-    html: content,
-    };
-
-    axios({
-        method: 'post',
-        url: 'http://localhost:5000/api/send',
-        data: msg
-    }).then(function(response) {
-        //console.log(response);
-        if(response.status === 200) {
-            res.json(response.data);
-        }
-    }).catch(function(error) {
-        //return error;
-        res.json(JSON.stringify(error));
-    });
 
 })
 
