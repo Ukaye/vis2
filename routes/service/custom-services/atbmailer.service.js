8d6eb2ff1307aa666039759c34533bf5fbe77e55
realprinceviper <idrisadeniji01@gmail.com>
2020-01-16 01:03:18 -0600

    create endpoint to send triggered email

--- routes/service/custom-services/atbmailer.service.js
+++ routes/service/custom-services/atbmailer.service.js
@@ -29,8 +29,83 @@ router.post('/trigger/save', function(req, res, next) {
         next
 })
 
+router.post('/trigger/send', function(req, res, next) {
 
+    const mailData = req.body
+
+    let query = `SELECT trigger_recipient, trigger_content FROM email_templates WHERE trigger_name = ${mailData.triggerName}`,
+        data = db.query(query, function(error, results) {
+            if(error) {
+                res.send({
+                    status: 500,
+                    error,
+                    result: null
+                })
+            }
+            return results
+        })
         
+    let sterilizedMsg = data.trigger_content,
+        subject = data.trigger_subject,
+        mentions = extract(sterilizedMsg, { unique: true, symbol: false});
+    let recipients = mailData.recipients;
+    let recipientsArray = (recipients).split(',');
+
+    recipientsArray.every((recipient)=> {
+    
+        //proposal
+        
+        if(mentions.length > 0) {
+            //let i = mentions.length - 1;
+            let i = 0;
+    
+            mentions.forEach(element => {
+                let query = `select ${element} from users where email = '${recipient}'`;
+                    db.query(query, function(error, results) {
+                        if(error) {
+                            return console.log(error);
+                        } else {
+                            results.map(liveData => {
+                                console.log(liveData[element]);
+                                //for(mentions[i] in unsterilizedMsg) 
+                                let regex = new RegExp('@'+element, 'gi');
+                                sterilizedMsg = sterilizedMsg.replace(regex, liveData[element]);
+                            });
+                            i++;
+    
+                            if(i === mentions.length) {
+                                //console.log(sterilizedMsg);
+    
+                                 let msg = {
+                                    to: recipient,
+                                    from: 'noreply@atbtechsoft.com',
+                                    subject,
+                                    text: 'hi',
+                                    html: sterilizedMsg,
+                                    };
+                                
+                                    axios({
+                                        method: 'post',
+                                        url: 'http://localhost:5000/api/send',
+                                        data: msg
+                                    }).then(function(response) {
+                                        //console.log(response);
+                                        if(response.status === 200) {
+                                            res.json(response.data);
+                                        }
+                                    }).catch(function(error) {
+                                        //return error;
+                                        res.json(JSON.stringify(error));
+                                    });
+    
+                                    console.log(sterilizedMsg);
+                            }
+                        }
+                    })
+            });
+    
+        }
+    })
 /*
 
     if(mailData.template !== '') {
@@ -125,7 +200,7 @@ router.post('/trigger/save', function(req, res, next) {
 
     //db.query(query);
 
-//})
+})
 
 
 module.exports = router;
