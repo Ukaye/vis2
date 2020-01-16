bfab7e4f1d3ec1220dbafab75420c17e0f0b222a
realprinceviper <idrisadeniji01@gmail.com>
2020-01-16 10:20:01 -0600

    update trigger content if name exists

--- routes/service/custom-services/atbmailer.service.js
+++ routes/service/custom-services/atbmailer.service.js
@@ -8,23 +8,49 @@ const emailService = require('./email.service')
 
 router.post('/trigger/save', function(req, res, next) {
 
-    //save trigger content to db
     let mailData = req.body,
-        query = `INSERT into email_templates SET trigger_name = '${mailData.triggerName}', trigger_subject = '${mailData.triggerSubject}', trigger_content = '${mailData.triggerContent}'`
-        db.query(query, function (error, response) {
-            if(error) {
-                res.send({
-                    status: 500,
-                    error,
-                    response: null
+
+        query = `SELECT count(*) as rows FROM email_templates WHERE trigger_name = '${mailData.triggerName.trim()}'`
+        db.query(query, function (error, results) {
+            if(results[0].rows > 0) {
+                //update content if trigger exists
+                query = `UPDATE email_templates SET trigger_subject = '${mailData.triggerSubject}', trigger_content = '${mailData.triggerContent}'`
+                db.query(query, function (error, results) {
+                    if(error) {
+                        res.send({
+                            status: 500,
+                            error,
+                            response: null
+                        })
+                    }
+        
+                    res.send({
+                        status: 200,
+                        error: null,
+                        response: "Trigger updated successfully"
+                    })
+                })
+            } else {
+
+                //save trigger if name does not exist
+                query = `INSERT into email_templates SET trigger_name = '${mailData.triggerName.trim()}', trigger_subject = '${mailData.triggerSubject}', trigger_content = '${mailData.triggerContent}'`
+                db.query(query, function (error, results) {
+                    if(error) {
+                        res.send({
+                            status: 500,
+                            error,
+                            response: null
+                        })
+                    }
+        
+                    res.send({
+                        status: 201,
+                        error: null,
+                        response: "Trigger saved successfully"
+                    })
                 })
             }
-
-            res.send({
-                status: 201,
-                error: null,
-                response: "Trigger saved successfully"
-            })
+            
         })
 
         next
