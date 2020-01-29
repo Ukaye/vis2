b52c2c12ce15040827acdf5c2489f1e635003ef1
realprinceviper <idrisadeniji01@gmail.com>
2020-01-29 01:59:53 -0600

    encode html data to prevent sql error while saving into db

--- routes/service/custom-services/atbmailer.service.js
+++ routes/service/custom-services/atbmailer.service.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const extract = require('mention-hashtag');
-const replaceOnce = require('replace-once')
+const replaceOnce = require('replace-once');
+const he = require('he')
 const fs = require('fs');
 const axios = require('axios');
 const db = require('../../../db');
@@ -15,14 +16,10 @@ router.post('/trigger/save', function(req, res, next) {
         db.query(query, function (error, results) {
             if(results[0].rows > 0) {
                 //update content if trigger exists
-                query = `UPDATE email_templates SET trigger_subject = '${mailData.triggerSubject}', trigger_content = '${mailData.triggerContent}' WHERE trigger_name = '${mailData.triggerName.trim()}'`
+                query = `UPDATE email_templates SET trigger_subject = '${mailData.triggerSubject}', trigger_content = '${he.encode(mailData.triggerContent)}' WHERE trigger_name = "${mailData.triggerName.trim()}"`
                 db.query(query, function (error, results) {
                     if(error) {
-                        res.send({
-                            status: 500,
-                            error,
-                            response: null
-                        })
+                        return console.log(error)
                     }
         
                     res.send({
@@ -34,7 +31,7 @@ router.post('/trigger/save', function(req, res, next) {
             } else {
 
                 //save trigger if name does not exist
-                query = `INSERT into email_templates SET trigger_name = '${mailData.triggerName.trim()}', trigger_subject = '${mailData.triggerSubject}', trigger_content = '${mailData.triggerContent}'`
+                query = `INSERT into email_templates SET trigger_name = '${mailData.triggerName.trim()}', trigger_subject = '${mailData.triggerSubject}', trigger_content = '${he.encode(mailData.triggerContent)}'`
                 db.query(query, function (error, results) {
                     if(error) {
                         res.send({
@@ -59,6 +56,70 @@ router.post('/trigger/save', function(req, res, next) {
 
 router.post('/trigger/send', function(req, res, next) {
 
+    const mailData = req.body
+
+    let query = `SELECT trigger_subject, trigger_content FROM email_templates WHERE trigger_name = '${mailData.triggerName}'`
+        db.query(query, function(error, results) {
+            if(error) {
+                res.send({
+                    status: 500,
+                    error,
+                    result: null
+                })
+            }
+
+            let data = results[0],
+                unsterilizedMsg = he.decode(data.trigger_content),
+                subject = data.trigger_subject,
+                mentions = extract(unsterilizedMsg, { unique: true, symbol: false});
+                mentionsWithSymbol = extract(unsterilizedMsg, { unique: true, symbol: true}),
+                mentionsList = mentions.toString(),
+                recipients = mailData.recipients;
+                recipientsArray = (recipients).split(','),
+                find,
+                replace,
+                sterilizedMsg;
+    
+                recipientsArray.forEach((recipient)=> {
+                    
+                    if(mentions.length > 0) {
+                        let query = `select ${mentionsList} from clients where email = '${recipient}'`;
+                        db.query(query, function(error, results) {
+                            if(error) {
+                                return console.log(error);
+                            } else {
+                                results.map(liveData => {
+                                    find = mentionsWithSymbol;
+                                    replace = Object.values(liveData)
+                                    sterilizedMsg = replaceOnce(unsterilizedMsg, find, replace, 'gi')
+                                    msg = {
+                                        to: recipient,
+                                        subject,
+                                        from: 'no-reply@app.finratus.com',
+                                        html: sterilizedMsg
+                                        };
+                                        emailService.sendHtmlByDomain(msg)
+                                });
+                            }
+                        })
+                    } else {
+
+                        msg = {
+                            to: recipient,
+                            subject,
+                            from: 'no-reply@app.finratus.com',
+                            html: unsterilizedMsg
+                            };
+                            emailService.sendHtmlByDomain(msg)                        
+                    }
+            })
+            res.status(200).send()
+        })
+
+})
+
+router.post('/mail/send', function(req, res, next) {
+
     let msg
     let mailData = req.body,
 
@@ -69,7 +130,6 @@ router.post('/trigger/send', function(req, res, next) {
                 mentionsList = mentions.toString(),
                 recipients = mailData.emailRecipients,
                 recipientsArray = (recipients).split(',');
-                console.log(unsterilizedMsg)
     
                 recipientsArray.forEach((recipient)=> {
                     
--- views/atbmailer/trigger.js
+++ views/atbmailer/trigger.js
@@ -1,14 +1,5 @@
 $(document).ready(function() {
         
-    // CKEDITOR.replace( 'emailContent' );
-
-    // CKEDITOR.config.mentions = [
-    //     {
-    //         feed: [ 'username', 'first_name', 'middle_name', 'last_name', 'fullname', 'date_created', 'phone', 'address', 'email', 'dob', 'marital_status', 'loan_officer', 'client_state', 'postcode', 'client_country', 'years_add', 'ownership', 'employer_name', 'industry', 'job', 'salary', 'job_country', 'off_address', 'off_state', 'doe', 'gender', 'branch', 'bank', 'account', 'account_name', 'bvn', 'client_description', 'name', 'web_address', 'state', 'country'],
-    //         minChars: 0,
-    //         marker: '@'
-    //     }
-    // ];
     $('#emailContent').summernote({
         focus: true,
         hint: {
@@ -24,27 +15,35 @@ $(document).ready(function() {
             }    
           }
         });
+        $('.note-icon-caret').hide();
+        $('.note-popover').hide();
+        $('.note-insert').hide();
+        $('.note-view').hide();
 })
 
 function saveTrigger() {
         let obj = {
             triggerName: $(triggerName).val(),
             triggerSubject: $(triggerSubject).val(),
-            triggerContent: CKEDITOR.instances.triggerContent.getData()
+            triggerContent: $('#emailContent').summernote('code')
         }
         
-        $.ajax({
-            'url': '/atbmailer/trigger/save/',
-            'type': 'post',
-            'data': obj,
-            'success': function (data) {
-                $(triggerName).val('');
-                $(triggerSubject).val('');
-                CKEDITOR.instances.triggerContent.setData('');
-                $('#wait').hide();
-                swal(data.response,"","success");
-            }
-        });
+        if(obj.triggerName === '' || obj.triggerSubject === '' || obj.triggerContent === '') {
+            return swal('Kindly fill all required fields!', '', 'warning');
+        } else {
+            $.ajax({
+                'url': '/atbmailer/trigger/save/',
+                'type': 'post',
+                'data': obj,
+                'success': function (data) {
+                    $(triggerName).val('');
+                    $(triggerSubject).val('');
+                    $('#emailContent').summernote('code', '');
+                    $('#wait').hide();
+                    swal(data.response,"","success");
+                }
+            });
+        }
     }
 
     function sendEmail() {
@@ -58,7 +57,7 @@ function saveTrigger() {
             return swal('Kindly fill all required fields!', '', 'warning');
         } else {
             $.ajax({
-                'url': '/atbmailer/trigger/send/',
+                'url': '/atbmailer/mail/send/',
                 'type': 'post',
                 'data': obj,
                 'success': function (data) {
