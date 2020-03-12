7dc8b795d81c181c4498ffb5cb377f0ef885e374
ukaye <itaukemeabasi@gmail.com>
2020-03-12 11:29:06 +0100

    req host debugging

--- routes/service/custom-services/preapproved-loan.service.js
+++ routes/service/custom-services/preapproved-loan.service.js
@@ -230,6 +230,11 @@ router.get('/recommendations/get/:id', function (req, res, next) {
  * 2. Direct Debit Mandate Setup
  */
 router.post('/create', function (req, res, next) {
+console.log(process.env.HOST)
+console.log(req.HOST)
+console.log(req.headers.origin)
+console.log(req.hostname)
+console.log(req.get('host'))
     const HOST = `${req.protocol}://${req.get('host')}`;
     let data = {},
         postData = Object.assign({},req.body.application),
