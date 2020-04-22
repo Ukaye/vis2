8bcaf23f9fdf48ba71de0fc4b2764f10909852e0
ukaye <itaukemeabasi@gmail.com>
2020-04-22 18:37:36 +0100

    firebase debugging

--- routes/service/custom-services/firebase.service.js
+++ routes/service/custom-services/firebase.service.js
@@ -5,9 +5,12 @@ const firebase = {},
     fcm = new FCM(serverKey);
 
 firebase.send = payload => {
+    console.log(payload)
     if (!payload.to) return;
     const query = `SELECT firebase_token FROM clients WHERE email = "${payload.to}"`;
     db.query(query, (error, client) => {
+        console.log(error)
+        console.log(client)
         client = client[0];
         if (!client || !client.firebase_token) return;
         var message = {
@@ -19,6 +22,9 @@ firebase.send = payload => {
             data: { }
         };
         fcm.send(message, (error, response) => {
+            console.log(error)
+            console.log(response)
+            console.log('===============================================================================================================================================================')
             if (error) console.log(error);
         });
     });
