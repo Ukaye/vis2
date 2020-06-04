bc4ea8123e43f78ea58b14c09b863caed755b51d
ukaye <itaukemeabasi@gmail.com>
2020-06-04 11:29:49 +0100

    remita debugging

--- routes/service/custom-services/client.service.js
+++ routes/service/custom-services/client.service.js
@@ -68,6 +68,7 @@ router.post('/mandate/setup', (req, res) => {
         mandateType: 'DD',
         maxNoOfDebits: '100'
     }, function (payload, setup_response) {
+        console.log(payload)
         console.log(setup_response)
         if (setup_response && setup_response.mandateId) {
             let authorize_payload = {
