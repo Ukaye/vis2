ac9f484b0be1a8819141e4fa7fe2f7903ea0986e
ukaye <itaukemeabasi@gmail.com>
2020-06-17 10:43:50 +0100

    firebase cleanup

--- routes/service/custom-services/firebase.service.js
+++ routes/service/custom-services/firebase.service.js
@@ -1,32 +1,10 @@
 const firebase = {},
-    // FCM = require('fcm-node'),
+    FCM = require('fcm-node'),
     db = require('../../../db'),
-    // serverKey = require('../../../firebase-key.json'),
-    // fcm = new FCM(serverKey);
     OneSignal = require('onesignal-node'),
+    serverKey = require('../../../firebase-key.json'),
+    fcm = new FCM(serverKey),
     oneSignal = new OneSignal.Client(process.env.ONESIGNAL_APP_ID, process.env.ONESIGNAL_API_KEY);
-
-// firebase.send = payload => {
-//     if (!payload.to) return;
-//     const query = `SELECT firebase_token FROM clients 
-//         WHERE username = "${payload.to}" OR email = "${payload.to}" OR phone = "${payload.to}"`;
-//     db.query(query, (error, client) => {
-//         client = client[0];
-//         if (!client || !client.firebase_token) return;
-//         let message = {
-//             to: client.firebase_token,
-//             notification: {
-//                 title: payload.subject,
-//                 body: (payload.context && payload.context.message)? payload.context.message : ''
-//             }
-//         };
-//         if (payload.data) message.data = payload.data;
-//         fcm.send(message, (error, response) => {
-//             if (error) console.log(error);
-//             console.log(response)
-//         });
-//     });
-// };
 
 firebase.send = payload => {
     if (!payload.to) return;
@@ -47,7 +25,28 @@ firebase.send = payload => {
         };
         if (payload.data) notification.data = payload.data;
         const response = await oneSignal.createNotification(notification);
-        console.log(response.body);
+    });
+};
+
+firebase.sendV2 = payload => {
+    if (!payload.to) return;
+    const query = `SELECT firebase_token FROM clients 
+        WHERE username = "${payload.to}" OR email = "${payload.to}" OR phone = "${payload.to}"`;
+    db.query(query, (error, client) => {
+        client = client[0];
+        if (!client || !client.firebase_token) return;
+
+        let message = {
+            to: client.firebase_token,
+            notification: {
+                title: payload.subject,
+                body: (payload.context && payload.context.message)? payload.context.message : ''
+            }
+        };
+        if (payload.data) message.data = payload.data;
+        fcm.send(message, (error, response) => {
+            if (error) console.log(error);
+        });
     });
 };
 
